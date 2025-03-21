0c9953be88e92d142832a4f5a0932f34ee02db93
ToShiK_06 <antonshorin23@gmail.com>
2025-03-21 13:38:28 +0300

    fvrvjrcn

--- src/components/poleVvoda/Text.js
+++ src/components/poleVvoda/Text.js
@@ -1,33 +1,29 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 const InputField4 = () => {
-
-  const [inputValue, setInputValue] = useState('');
-
+  const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
-    setInputValue(event.target.value);
-    
-  };
+    const value = event.target.value;
 
+    if (/^[smwm]*$/.test(value)) {
+      setInputValue(value);
+    }
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault(); 
-   
+    event.preventDefault();
   };
   const inputStyle = {
-    border: '2px solid rgb(144, 139, 139)',
-    borderRadius: '20px',
-    padding: '10px',
-    fontSize: '18px',
-    width: '210px',
-    height: '35px',
-    display: 'flex',
-    
-    marginLeft: '5px', 
-    
-   
+    border: "2px solid rgb(144, 139, 139)",
+    borderRadius: "20px",
+    padding: "10px",
+    fontSize: "18px",
+    width: "210px",
+    height: "35px",
+    display: "flex",
+
+    marginLeft: "5px",
   };
 
   return (
@@ -37,12 +33,11 @@ const InputField4 = () => {
           type="text"
           style={inputStyle}
           value={inputValue}
+          maxLength={4}
           onChange={handleChange}
           placeholder="Введите капчу"
         />
-       
       </form>
-   
     </div>
   );
 };
